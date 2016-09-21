1b1fa5652e05b423a038f543cbcea951f41e4296
Tracy Lee <tracy@wingman.events>
2016-09-21 07:38:59 -0400

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: [],
+  template: 'hi'
 })
 export class AppComponent {
   title = 'app works!';
